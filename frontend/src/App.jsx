ff85f421c0c5a8179d9875c6bb6cd98e87d548d8
Manjeet Singh <118804835+ManjeetAulakh@users.noreply.github.com>
2025-09-26 01:28:17 +0530

    fix login signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -64,6 +64,7 @@ function App() {
             <div className="App">
               <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route
                 path="/dashboard"
