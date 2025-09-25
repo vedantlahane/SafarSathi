16c9cb906668547e29b2bdf442bcb42150d01137
Manjeet Singh <118804835+ManjeetAulakh@users.noreply.github.com>
2025-09-26 02:40:25 +0530

    okkk

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
+  define: {
+    // Defines 'global' as 'window' for libraries that need it (like sockjs-client)
+    global: 'window', 
+  },
+
 })
