e4ad34d944241059ae887d0058a232a678b61c22
Vedant Lahane <vedantanillahane@gmail.com>
2025-12-14 23:04:41 +0530

    feat: Add initial Home, Map, Identity, and Settings pages, integrate them into a tabbed mobile layout, and update styling and build configurations.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,9 @@
-import { Button } from "./components/ui/button"
 import MobileLayout from "./layout/MobileLayout"
 function App() {
 
 
   return (
     <>
-      <p className="text-8xl">fuck this</p>
-      <Button>Click Me</Button>
       <MobileLayout/>
     </>
   )
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,9 +13,10 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-   resolve: {
+  resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
+
 })
